fbf868528ddba383c2aaf7e227fa12bebf5a2f00
srinashg <51180003+srinashg@users.noreply.github.com>
2025-03-26 22:36:24 -0500

    created /add-engine endpoint

--- emission_analyzer_api/urls.py
+++ emission_analyzer_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import addUser
+from .views import addUser, addEngine #importing the methods from views.py which are used to parse the json data and return the response
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("add-user/", addUser, name="add_user")
+    path("add-user/", addUser, name="add_user"),
+    path("add-engine/", addEngine, name="add_engine")
 ]
