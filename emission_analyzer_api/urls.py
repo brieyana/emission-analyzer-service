e835047513d7df3cdc5335c51ffe653ef5c8577c
Srinesh Ganesh <srinashg@gmail.com>
2025-04-02 19:17:50 -0500

    getting user engines

--- emission_analyzer_api/urls.py
+++ emission_analyzer_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import addUser, addEngine, getUser #importing the methods from views.py which are used to parse the json data and return the response
-
+#importing the methods from views.py which are used to parse the json data and return the response
+from .views import addUser, addEngine, getUser, getEngines
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("add-user/", addUser, name="add_user"),
     path("add-engine/", addEngine, name="add_engine"),
-    path("get_user/<str:user_id>", getUser, name="get_user")
+    path("get_user/<str:user_id>", getUser, name="get_user"),
+    path("get-engines/<str:user_id>", getEngines, name="get_engines")
 ]
