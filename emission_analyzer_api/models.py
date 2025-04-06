2593f34cb53c2c00a10ab3b198245c542d466f81
Brie Haynes <briehaynes9@gmail.com>
2025-04-06 12:47:48 -0500

    Updated constraints for engine model

--- emission_analyzer_api/models.py
+++ emission_analyzer_api/models.py
@@ -12,8 +12,16 @@ class EngineType(models.Model):
 class Engine(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     engine_type = models.ForeignKey(EngineType, on_delete=models.SET_NULL, null=True)
-    engine_identification = models.CharField(max_length=100, null=False)
+    engine_identification = models.CharField(max_length=100, null=False, unique=False)
     rated_thrust = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     bp_ratio = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     pressure_ratio = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     created_at = models.DateTimeField("date created", null=False, default=timezone.now)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'engine_identification'], 
+                name='unique_user_engine_identification'
+            )
+        ]
